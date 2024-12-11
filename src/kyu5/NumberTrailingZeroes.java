be21c393b31bad42d56bef7c62357d7ede37d3ab
chaika_sv <chaika_sv@mail.ru>
2024-12-11 15:33:42 +0300

    Number of trailing zeros of N!

--- src/kyu5/NumberTrailingZeroes.java
+++ src/kyu5/NumberTrailingZeroes.java
@@ -1,2 +1,35 @@
-package kyu5;public class NumberTrailingZeroes {
+package kyu5;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class NumberTrailingZeroes {
+
+
+    public static void main(String[] args) {
+        System.out.println( zeros(14) );
+    }
+
+
+    public static int zeros(int n) {
+
+        int res = 0;
+        int factor5 = 5;
+
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+
+        do {
+            map.put(factor5, n / factor5);
+            factor5 *= 5;
+        }
+        while (factor5 < n);
+
+        for (var entry : map.entrySet()) {
+            res += entry.getValue();
+        }
+
+        return res;
+    }
+
+
 }
