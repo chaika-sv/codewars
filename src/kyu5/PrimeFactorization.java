c78b2604b9f1d6c93fc7f7715ed839680997db65
chaika_sv <chaika_sv@mail.ru>
2025-10-03 15:29:51 +0300

    New katas

--- src/kyu5/PrimeFactorization.java
+++ src/kyu5/PrimeFactorization.java
@@ -1,2 +1,39 @@
-package kyu5;public class PrimeFactorization {
+package kyu5;
+
+public class PrimeFactorization {
+
+
+    public static void main(String[] args) {
+        System.out.println( factors(933555431 ) );
+    }
+
+
+    public static String factors(int n) {
+        StringBuilder res = new StringBuilder();
+        int power = 0;
+
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                power++;
+                n /= i;
+            }
+            if (power == 0) {
+                continue;
+            }
+            res.append("(").append(i);
+            if (power > 1) {
+                res.append("**").append(power);
+            }
+            res.append(")");
+            power = 0;
+        }
+
+        if (n != 1) {
+            res.append("(").append(n).append(")");
+        }
+
+        return res.toString();
+    }
+
+
 }
--- src/kyu5/Rot13.java
+++ src/kyu5/Rot13.java
@@ -1,2 +1,42 @@
-package kyu5;public class Rot13 {
+package kyu5;
+
+public class Rot13 {
+
+
+    public static void main(String[] args) {
+        System.out.println( rot13("Test" ) );
+    }
+
+    public static String rot13(String str) {
+
+        StringBuilder result = new StringBuilder();
+        char[] chars = str.toCharArray();
+        char newChar;
+        int code;
+        int newCode;
+
+        for(char c : chars) {
+
+            code = (int)c;
+
+            // A..M, a..m
+            if ((code >= 65 && code <= 77) || (code >= 97 && code <= 109))
+                newCode = code + 13;
+                // N..Z
+            else if ((code >= 78 && code <= 90) || (code >= 110 && code <= 122))
+                newCode = code - 13;
+            else
+                newCode = code;
+
+            newChar = (char) newCode;
+
+            //System.out.println(c + " " + (int)c);
+            //System.out.println(newChar + " " + (int)newCode);
+
+            result.append(newChar);
+        }
+
+        return result.toString();
+    }
+
 }
