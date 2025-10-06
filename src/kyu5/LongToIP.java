3880b81cdc15096460cdfc51f30e4fc924b6e1fd
chaika_sv <chaika_sv@mail.ru>
2025-10-06 10:47:10 +0300

    LongToIP

--- src/kyu5/LongToIP.java
+++ src/kyu5/LongToIP.java
@@ -1,2 +1,24 @@
-package kyu5;public class LongToIP {
+package kyu5;
+
+public class LongToIP {
+
+
+    public static void main(String[] args) {
+        System.out.println( longToIP( 2154959208L ) );
+    }
+
+    public static String longToIP(long ip) {
+
+        String binary32 = String.format("%32s", Long.toBinaryString(ip)).replace(' ', '0');
+
+        String res =
+                (int) Long.parseLong( binary32.substring(0,8), 2) + "." +
+                (int) Long.parseLong( binary32.substring(8,16), 2) + "." +
+                (int) Long.parseLong( binary32.substring(16,24), 2) + "." +
+                (int) Long.parseLong( binary32.substring(24,32), 2);
+
+        return res;
+    }
+
+
 }
